a3563b58a4d784cae0e90a4793bf67cda296f32a
赵威 <zhaowei@qfpay.com>
2016-12-26 20:04:48 +0800

    change

--- data/banian.sql
+++ data/banian.sql
@@ -5,4 +5,28 @@ CREATE TABLE profile (
 	userid bigint(20) not null primary key
 );
 
+CREATE TABLE role (
+	id bigint(20) not null primary key,
+	name varchar(256) not null unique,
+	perm varchar(256)
+);
 
+CREATE TABLE team (
+	id bigint(20) not null primary key,
+	userid bigint(20) not null,
+	roleid bigint(20) not null,
+	ctime int(4) unsigned not null,
+	uptime int(4) unsigned not null
+);
+
+CREATE TABLE project (
+	id bigint(20) not null primary key,
+	name varchar(256) not null,
+	creatid bigint(20) not null comment '创建人id',
+	ctime int(4) unsigned not null,
+	uptime int(4) unsigned not null,
+);
+
+CREATE TABLE plan (
+	id bigint(20) not null primary key,
+);
