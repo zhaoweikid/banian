f59704d3a510ed6876f5787f69ad2ec2fac3b531
zhaoweikid <zhaoweikid@gmail.com>
2016-12-26 22:33:59 +0800

    change

--- data/banian.sql
+++ data/banian.sql
@@ -29,4 +29,40 @@ CREATE TABLE project (
 
 CREATE TABLE plan (
 	id bigint(20) not null primary key,
+	pmid bigint(20) not null,
+	masterid bigint(20) not null,
+	ctime int(4) unsigned not null,
+	uptime int(4) unsigned not null
 );
+
+CREATE TABLE plan_ref (
+	id bigint(20) not null primary key,
+	planid bigint(20) not null,
+	refid bigint(20) not null
+);
+
+
+CREATE TABLE items (
+	id bigint(20) not null primary key,
+	title varchar(256) not null,
+	content varchar(4096),
+	attach varchar(2048),
+	memo varchar(2048),
+	creatid bigint(20) not null,
+	state smallint not null default 1,
+	ctime int(4) unsigned not null,
+	uptime int(4) unsigned not null
+);
+
+CREATE TABLE comments (
+	id bigint(20) not null primary key,
+	itemid bigint(20) not null,
+	creatid bigint(20) not null,
+	content varchar(2048),
+	ctime int(4) unsigned not null,
+	uptime int(4) unsigned not null
+);
+
+
+
+
